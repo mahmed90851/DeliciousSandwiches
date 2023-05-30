b9ebcc8a2b4abb6457ad47497cf16f41507549a3
nabilhussein <55296823+nabilhussein@users.noreply.github.com>
2023-05-30 13:40:17 -0400

    drink class

---
+++ src/main/java/org/example/Drink.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Drink {
+    private int size;
+    private String flavor;
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public Drink(int size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+}
