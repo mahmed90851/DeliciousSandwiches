5b17e860083d97f311b0ac378b436b8a4d3edbdf
tchou0 <tchou@appdev.yearup.org>
2023-06-01 09:57:50 -0400

    added ColorCodes class

---
+++ src/main/java/org/example/ColorCodes.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class ColorCodes {
+
+    // TEXT - COLORS
+    public static final String ANSI_YELLOW="\u001B[93m";
+    public static final String ANSI_RESET="\u001B[0m";
+    public static final String ANSI_BLUE="\u001B[96m";
+    public static final String ANSI_PINK="\u001B[96m";
+    public static final String TEXT_RED = "\u001B[31m";
+    public static final String TEXT_BLACK = "\u001B[30m";
+    public static final String TEXT_GREEN = "\u001B[32m";
+    public static final String TEXT_BLUE = "\u001B[34m";
+    public static final String TEXT_RESET = "\u001B[0m";
+    public static final String TEXT_PURPLE = "\u001B[35m";
+    public static final String TEXT_CYAN = "\u001B[36m";
+    public static final String TEXT_YELLOW = "\u001B[33m";
+    public static final String TEXT_WHITE = "\u001B[37m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ORANGE = "\u001B[34m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+
+}
